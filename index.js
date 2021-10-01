c0e6d55b0729338c8b376011f072911910dca039
KKD009 <83933652+KKD009@users.noreply.github.com>
2021-10-01 11:39:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
-  return res.end(`Hello streamers! This is showing that auto-deployments from Github are a success v2`);
+  return res.end(`Hello streamers! This is showing that auto-deployments from Github are a success v3`);
 });
 
 server.listen(PORT, () => {
